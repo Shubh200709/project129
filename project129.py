ba264044af5aa39c032199fdd506150c847822ee
Shubh200709 <79042176+Shubh200709@users.noreply.github.com>
2023-05-07 09:45:26 +0530

    Add files via upload

---
+++ project129.py
@@ -0,0 +1,42 @@
+import csv
+import pandas as pd
+
+with open('Dwarf_Stars.csv') as f:
+    data = csv.reader(f)
+    data_list = list(data)
+
+
+
+m = data_list.pop(3)
+r = data_list.pop(4)
+
+star_name = data_list[1:]
+
+headers = ['','star_name','distance','mass','radius']
+# print(star_name)
+
+for i in star_name:
+    new_s = i[3]
+    if new_s.isdigit():
+        mas = float(new_s)*0.102763
+    else:
+        mas = ''
+data_list.append(mas)
+
+for i in star_name:
+    new_s = i[4]
+    if new_s.isdigit():
+        rad = float(new_s)*0.000954588
+    else:
+        rad = ''
+data_list.append(rad)
+
+data_list.pop(0)
+
+# with open('project129output(6).csv','w',encoding='utf8') as f:
+#     data1 = csv.writer(f)
+#     data1.writerow(headers)
+#     data1.writerows(data_list)
+
+df = pd.read_csv('project129output(6).csv')
+df = df[df['Distance, Mass, Radius'].notna()]
